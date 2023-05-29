a727def3dd79c74c56b25616ea5672f5605e399a
Victor Hugo <victorblog410@gmail.com>
2023-05-28 22:26:25 -0300

    adding query user and test http for endpoints

--- sql/queries/user.sql
+++ sql/queries/user.sql
@@ -27,3 +27,13 @@ SELECT * FROM users WHERE email = $1;
 
 -- name: GetUserRoleName :one
 SELECT ur.name FROM user_roles ur JOIN users u ON u.role_id = ur.id WHERE u.id = $1;
+
+
+-- name: GetUserCompleteProfile :one
+SELECT u.id, u.username, u.email, u.role_id, u.active,
+       g.id AS gym_id, g.gym_name, g.team_name, g.active AS gym_active,
+       s.id AS student_id, s.graduation, s.active AS student_active, s.training_time
+FROM users u
+LEFT JOIN gyms g ON u.id = g.user_id
+LEFT JOIN students s ON g.id = s.gym_id
+WHERE u.id = $1;
